2c5c31dff8aaf7a7591d3186108084d8ddaea1c6
ramyses <lukasramysesleao@gmail.com>
2024-09-24 12:51:56 -0400

    create styles notfound

--- src/pages/notfound/index.tsx
+++ src/pages/notfound/index.tsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom'
+import styles from './notfound.module.css'
+
 export function Notfound(){
   return(
-    <div>
+    <div className={styles.container}>
       <h1>
-        Not Found
+        Pagina 404 não existe
       </h1>
+      <Link to="/">Acessar cripto moedas </Link>
     </div>
   )
 }
